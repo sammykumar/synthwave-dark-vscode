97fd924cd5497a12b7a73ee542c0864e519be9ab
Sam Kumar <sammy.kumar@icloud.com>
2025-05-31 09:47:31 -0500

    📝 (extension.ts): update console log message to reflect the glow effect feature and remove unused command registration for helloWorld

--- src/extension.ts
+++ src/extension.ts
@@ -18,16 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "synthwave-dark-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('synthwave-dark.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Synthwave Dark!');
-	});
+	console.log('Run Synthwave Dark: Enable Glow to enable glow effect');
 
 	// Register the test command defined in package.json
 	const enableGlowDisposable = vscode.commands.registerCommand('synthwave-dark.enableGlow', async () => {
@@ -49,7 +40,6 @@ export function activate(context: vscode.ExtensionContext) {
 		cleanUpWorkbench(workbenchHtmlPath);
 	});
 
-	context.subscriptions.push(disposable);
 	context.subscriptions.push(enableGlowDisposable);
 	context.subscriptions.push(cleanUpWorkbenchDisposable);
 
