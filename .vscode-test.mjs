db83e714d2e6fbb141a99e3568ca1e6195d9e93f
Sam Kumar <sammy.kumar@icloud.com>
2025-06-01 06:38:32 -0500

    ✨ (vscode-test.mjs): add workspaceFolder and userDataDir to configure test environment ✨ (package.json): add clean and clean-test scripts to remove build artifacts 📝 (package.json): update pretest script to include clean-test for a fresh build 📝 (tsconfig.json): add outDir to specify output directory for compiled files

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "@vscode/test-cli";
 export default defineConfig({
   label: "unitTests",
   files: "out/test/**/*.test.js",
-  //   workspaceFolder:
-  //     "/Users/samkumar/Development/SK-Productions-LLC/synthwave-dark-vscode",
+  workspaceFolder:
+    "/Users/samkumar/Development/SK-Productions-LLC/synthwave-dark-vscode",
+  userDataDir: "/tmp/vsc-test-data",
 });
